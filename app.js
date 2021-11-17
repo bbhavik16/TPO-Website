4d84b3c37c9355edc9dd5f7eaf202408ef0480d7
SATYAM1548 <sbindroo_b20@ce.vjti.ac.in>
2021-11-17 20:54:56 +0530

    header not responsive made

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const path = require('path');
 const ejs = require('ejs');
 const ejsMate = require('ejs-mate');
 
+<<<<<<< Updated upstream
 app.engine('ejs', ejsMate);
 app.set("view engine", "ejs")                      //For EJS 
 app.set("views", path.join(__dirname, "views"))    //
@@ -19,12 +21,37 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
     res.render('/home');
+=======
+app.engine('ejs', ejsMate)
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+//app.use(methodOverride("_method"))                        //For overriding post with delete or edit request
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res) => {
+    res.render('layout/boilerplate')
+})
+app.get('/home', (req, res) => {
+    res.render('index');
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+>>>>>>> Stashed changes
 })
 
 app.get('/students', (req, res) => {
     res.render('students/index')
 })
 
+<<<<<<< Updated upstream
 app.listen(3000, () => { 
     console.log("Listening on port 3000"); 
 })
+=======
+
+
+
+>>>>>>> Stashed changes
