ae9b4b06d7f955b23e64c00f2d13c926af36e6bf
SATYAM1548 <sbindroo_b20@ce.vjti.ac.in>
2021-11-18 21:08:35 +0530

    footer responsive done

--- app.js
+++ app.js
@@ -4,27 +4,13 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const path = require('path');
 const ejs = require('ejs');
-const ejsMate = require('ejs-mate');
 
-<<<<<<< Updated upstream
+
+
 app.engine('ejs', ejsMate);
 app.set("view engine", "ejs")                      //For EJS 
 app.set("views", path.join(__dirname, "views"))    //
 
-app.use(express.urlencoded({ extended: true }))
-//app.use(methodOverride("_method"))                        //For overriding post with delete or edit request
-app.use(express.static(path.join(__dirname, "public"))) 
-
-app.get('/', (req, res) => {
-     res.render('layout/boilerplate')
-})
-
-app.get('/home', (req, res) => {
-    res.render('/home');
-=======
-app.engine('ejs', ejsMate)
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 //app.use(methodOverride("_method"))                        //For overriding post with delete or edit request
 app.use(express.static(path.join(__dirname, "public")))
@@ -32,6 +18,11 @@ app.use(express.static(path.join(__dirname, "public")))
 app.get('/', (req, res) => {
     res.render('layout/boilerplate')
 })
+
+app.get('/students', (req, res) => {
+    res.render('students/index')
+})
+
 app.get('/home', (req, res) => {
     res.render('index');
 })
@@ -39,19 +30,11 @@ app.get('/home', (req, res) => {
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
->>>>>>> Stashed changes
+
 })
 
-app.get('/students', (req, res) => {
-    res.render('students/index')
-})
-
-<<<<<<< Updated upstream
-app.listen(3000, () => { 
-    console.log("Listening on port 3000"); 
-})
-=======
 
 
 
->>>>>>> Stashed changes
+
+
